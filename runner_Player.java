0fd5cd73474856376c48eb77a2cdbc895992bf54
Mr. Mueller <bmueller@hstat.org>
2021-05-10 22:52:21 +0000

    reset

--- runner_Player.java
+++ runner_Player.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class runner_Player {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 
 }
